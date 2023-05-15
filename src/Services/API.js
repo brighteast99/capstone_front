779ec4fc0e6cb40255078a16b93456f140608a27
brighteast99 <brighteast99@gmail.com>
2023-05-15 19:20:16 +0900

    API 리졸버 작업

---
+++ src/Services/API.js
@@ -0,0 +1,42 @@
+import HTTP from "./HTTP";
+
+const API = {
+  Test: "test",
+  SignIn: "signin_user",
+  SignUp: "signup_user",
+  GetUser: "user",
+  GetUsers: "users",
+  EditUser: "edit_user",
+};
+Object.freeze(API);
+
+/**
+ *
+ * @param {string} name - API 이름
+ * @param {Object} args - API와 전달할 인자
+ * @param {string | string[]} fields - 반환받을 필드
+ *
+ * @returns Axios Promise
+ */
+const apiRequest = (name, args, fields) => {
+  let argString = "";
+  for (const key of Object.keys(args)) {
+    //Wrap value with "" if type is string
+    if (typeof args[key] == "string") argString += `${key}:"${args[key]}"`;
+    else argString += `${key}:${args[key]}`;
+    argString += " ";
+  }
+  argString = argString.trimEnd();
+
+  let fieldString = "";
+  if (typeof fields == "object")
+    fieldString = fields.reduce((accum, current) => `${accum} ${current}`);
+  else if (fields) fieldString = fields;
+
+  let queryString = `{${name}(${argString})${
+    fieldString.length > 0 ? ` { ${fieldString} }` : ""
+  }}`;
+  return HTTP.post("", { query: queryString });
+};
+
+export { API, apiRequest };
